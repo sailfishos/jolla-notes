53d34a596c899149ff11549355f28d894ea115ec
Joona Petrell <joona.petrell@jollamobile.com>
2015-11-27 12:00:16 +0200

    [license] Unify proprietary licenses. Contributes to JB#26864
    Revert "Prepare for open sourcing. Contributes to JB#30968."
    
    This reverts commit e8a1592891e59e0cfa757f153458e3328b89ebb7.
    
    Conflicts:
    	qml/notes.js
    	settings/notes.qml
    	settings/plugin.cpp

--- qml/notes.js
+++ qml/notes.js
@@ -1,20 +1,5 @@
-/*
- * Copyright (C) 2012-2015 Jolla Ltd.
- *
- * The code in this file is distributed under multiple licenses, and as such,
- * may be used under any one of the following licenses:
- *
- *   - GNU General Public License as published by the Free Software Foundation;
- *     either version 2 of the License (see LICENSE.GPLv2 in the root directory
- *     for full terms), or (at your option) any later version.
- *   - GNU Lesser General Public License as published by the Free Software
- *     Foundation; either version 2.1 of the License (see LICENSE.LGPLv21 in the
- *     root directory for full terms), or (at your option) any later version.
- *   - Alternatively, if you have a commercial license agreement with Jolla Ltd,
- *     you may use the code under the terms of that license instead.
- *
- * You can visit <https://sailfishos.org/legal/> for more information
- */
+// Copyright (C) 2012-2013 Jolla Ltd.
+// Contact: Richard Braakman <richard.braakman@jollamobile.com>
 
 // The page numbers in the db must stay sequential (starting from 1),
 // but the page numbers in the model may have gaps if the filter is active.
--- rpm/jolla-notes.spec
+++ rpm/jolla-notes.spec
@@ -3,7 +3,7 @@ Summary:    Note-taking application
 Version:    0.8.57
 Release:    1
 Group:      Applications/Editors
-License:    LGPL-2.1+ OR GPL-2.0+ OR SailfishCommercial
+License:    Proprietary
 URL:        https://bitbucket.org/jolla/ui-jolla-notes
 Source0:    %{name}-%{version}.tar.bz2
 BuildRequires:  pkgconfig(Qt5Core)
@@ -55,8 +55,6 @@ and a test definition XML file for testrunner-lite.
 
 %package settings
 Summary:   Setting page for jolla-notes
-License:   LGPL-2.1+ OR GPL-2.0+ OR SailfishCommercial
-Group:     System/Applications
 Requires:  jolla-settings
 
 %description settings
